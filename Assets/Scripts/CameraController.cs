c00f14521deb9ea7c54c0b0c617a205084bd3e74
jonasCCa <jonas.ccampos@hotmail.com>
2021-12-24 12:42:44 -0300

    v0.1d - Camera Tweaks and Manager Split
    Added:
    - Camera has a new distance value for Singleplayer
    
    Changed:
    - Camera no longer tilts while following a target
    - Split MultiplayerManager into another class, CameraController, for modularity purposes

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Object References")]
+    public GameObject gameCamera;
+    public GameObject cameraTarget;
+    [SerializeField] private MultiplayerManager manager;
+
+    [Header("Camera Configs")]
+    public float distanceRatio = .5f;
+    public float singlePlayerDistance = 5f;
+    public float maxCameraDistance = 10f;
+    public float cameraSmoothTime = 0.2f;
+    public float cameraMaxSpeed = 50f;
+    public Vector3 cameraOffset = new Vector3(0,3.15f,-3.99f);
+    
+    [Header("Read Only")]
+    [SerializeField] private bool isSinglePlayer = true;
+    [SerializeField] private float longestPlayerDistance;
+    [SerializeField] private Vector3 targetVelocity = Vector3.zero;
+    [SerializeField] private Vector3 cameraVelocity = Vector3.zero;
+    [SerializeField] private Vector3 cameraTargetPosition = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameCamera = GameObject.Find("MainCamera");
+        manager = GetComponent<MultiplayerManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+        // Calculates camera target position
+        cameraTargetPosition = manager.calculateTargetPosition();
+
+        // Smooths camera target position
+        cameraTarget.transform.position = Vector3.SmoothDamp(cameraTarget.transform.position, cameraTargetPosition,
+                                                            ref targetVelocity, cameraSmoothTime, cameraMaxSpeed, Time.deltaTime);
+        
+        // Smooths camera position
+        gameCamera.transform.position = Vector3.SmoothDamp(gameCamera.transform.position, calculateCameraPosition() + cameraOffset,
+                                                            ref cameraVelocity, cameraSmoothTime, cameraMaxSpeed, Time.deltaTime);
+        // Points camera to target
+        gameCamera.transform.LookAt(cameraTarget.transform);
+    }
+
+    Vector3 calculateCameraPosition() {
+        Vector3 auxResult = new Vector3();
+
+        // If it's SinglePlayer, use single player distance, if else use distance ratio
+        if(isSinglePlayer)
+            auxResult = Vector3.ClampMagnitude(gameCamera.transform.forward*(singlePlayerDistance), maxCameraDistance);
+        else {
+            // Calculates the longest distance between players
+            longestPlayerDistance = manager.calculatePlayerDistance();
+
+            auxResult = Vector3.ClampMagnitude(gameCamera.transform.forward*(longestPlayerDistance * distanceRatio), maxCameraDistance);
+        }
+        return cameraTarget.transform.position - auxResult;
+    }
+
+    public void setSinglePlayerFlag(bool value) {
+        isSinglePlayer = value;
+    }
+
+    // Draws lines between centroid and players
+    void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        foreach(GameObject player in manager.GetPlayerList()) {
+            Gizmos.DrawLine(cameraTarget.transform.position, player.transform.position);
+        }
+    }
+}
--- Assets/Scripts/MultiplayerManager.cs
+++ Assets/Scripts/MultiplayerManager.cs
@@ -8,30 +8,15 @@ public class MultiplayerManager : MonoBehaviour
 {
     [Header("Object References")]
     [SerializeField] private PlayerInputManager playerInputManager;
-    public GameObject gameCamera;
     [SerializeField] private List<GameObject> playerList;
-    [SerializeField] private float longestPlayerDistance;
+    [SerializeField] private CameraController cController;
 
-    [Header("Camera Configs")]
-    public float distanceRatio = 1f;
-    public float maxCameraDistance = 15f;
-    public float cameraSmoothTime = 0.05f;
-    public float cameraMaxSpeed = 50f;
-    public Vector3 cameraOffset = new Vector3(0,3.15f,-3.99f);
-    public GameObject cameraTarget;
-    
-    [Header("Read Only")]
-    [SerializeField] private Vector3 targetVelocity = Vector3.zero;
-    [SerializeField] private Vector3 cameraVelocity = Vector3.zero;
-    [SerializeField] private Vector3 cameraOffsettedPosition = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
     {
         playerInputManager = GetComponent<PlayerInputManager>();
-        gameCamera = GameObject.Find("MainCamera");
-
-        
+        cController = GetComponent<CameraController>();
     }
 
     // Update is called once per frame
@@ -40,33 +25,8 @@ public class MultiplayerManager : MonoBehaviour
         
     }
 
-    void FixedUpdate() {
-        // Calculates camera target position
-        cameraOffsettedPosition = calculateTargetPosition();
-        // Calculates the longest distance between players
-        longestPlayerDistance = calculatePlayerDistance();
-
-        // Smooths camera target position
-        cameraTarget.transform.position = Vector3.SmoothDamp(cameraTarget.transform.position, calculateTargetPosition(),
-                                                            ref targetVelocity, cameraSmoothTime, cameraMaxSpeed, Time.deltaTime);
-        // Points camera to target
-        gameCamera.transform.LookAt(cameraTarget.transform);
-        // Smooths camera position
-        gameCamera.transform.position = Vector3.SmoothDamp(gameCamera.transform.position, calculateCameraPosition() + cameraOffset,
-                                                            ref cameraVelocity, cameraSmoothTime, cameraMaxSpeed, Time.deltaTime);
-
-    }
-
-    Vector3 calculateCameraPosition() {
-        Vector3 auxResult = new Vector3();
-
-        auxResult = Vector3.ClampMagnitude(gameCamera.transform.forward*(calculatePlayerDistance() * distanceRatio), maxCameraDistance);
-
-        return cameraOffsettedPosition - auxResult;
-    }
-
     // Calculates centroid between all players
-    Vector3 calculateTargetPosition() {
+    public Vector3 calculateTargetPosition() {
         Vector3 newTarget = new Vector3();
 
         foreach(GameObject player in playerList) {
@@ -81,7 +41,7 @@ public class MultiplayerManager : MonoBehaviour
 
     // Calculates the longest distance between players
     //   -> Complexity O(n² - n), gotta test performance
-    float calculatePlayerDistance() {
+    public float calculatePlayerDistance() {
         float longestDistance = 0;
 
         for(int i=0; i<playerList.Count; i++) {
@@ -98,11 +58,17 @@ public class MultiplayerManager : MonoBehaviour
         return longestDistance;
     }
 
-    // Adds Player to list when joined
+    // Adds Player to list when joined, set SinglePlayer flag on CameraController
     public void OnPlayerJoined(PlayerInput pI) {
         playerList.Add(pI.gameObject);
 
         Debug.Log("Player Joined; ID: " + pI.playerIndex);
+
+        if(playerList.Count == 1)
+            cController.setSinglePlayerFlag(true);
+        else
+            cController.setSinglePlayerFlag(false);
+
     }
 
     // Removes Player to list when left
@@ -114,13 +80,16 @@ public class MultiplayerManager : MonoBehaviour
         }
 
         Debug.Log("Player Left; ID: " + pI.playerIndex);
+
+        if(playerList.Count == 1)
+            cController.setSinglePlayerFlag(true);
+        else
+            cController.setSinglePlayerFlag(false);
     }
 
-    // Draws lines between centroid and players
-    void OnDrawGizmos() {
-        Gizmos.color = Color.red;
-        foreach(GameObject player in playerList) {
-            Gizmos.DrawLine(cameraTarget.transform.position, player.transform.position);
-        }
+
+
+    public List<GameObject> GetPlayerList() {
+        return playerList;
     }
 }
